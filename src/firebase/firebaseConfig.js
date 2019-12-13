28e8a9f153a8a8b39a1bd2a22041e0b30aa90dce
artemradz <artemradzivon@gmail.com>
2019-12-13 13:02:09 +0200

    add firebaseConfig

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+  export const firebaseConfig = {
+    apiKey: "AIzaSyC26Qx-dIx7x0QleTLFpRc0dBOiCNEhtDc",
+    authDomain: "chat-b7f03.firebaseapp.com",
+    databaseURL: "https://chat-b7f03.firebaseio.com",
+    projectId: "chat-b7f03",
+    storageBucket: "chat-b7f03.appspot.com",
+    messagingSenderId: "650968184019",
+    appId: "1:650968184019:web:07551fc2e6bce322"
+  };
